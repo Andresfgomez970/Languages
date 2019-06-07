b5212a0b257ea2381a1e1096c1747303d68f6415
Andres Gomez <andresf.gomez970@gmail.com>
2019-06-07 13:09:35 -0500

    1

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-git commit
-git remote add origin https://github.com/Andresfgomez970/Languages/tree/master
-git remote -v
+git commit -m "1"
+#git remote add origin https://github.com/Andresfgomez970/Languages/tree/master
+#git remote -v
 git push origin master
